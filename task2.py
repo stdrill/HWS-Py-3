2ec85eb05841ef33d97446dd858b24c49258ee08
stdrill <tanya23.96@mail.ru>
2022-10-17 21:46:13 +0300

    task 2 added

---
+++ task2.py
@@ -0,0 +1,14 @@
+# Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+nums = [23, 4, 3, 8, 2]
+
+def Multiplication_elements (nums):
+    res = []
+    for i in range(int(len(nums) / 2)):
+        res.append(nums[i] * nums[int(len(nums) - i - 1)])
+    if len(nums) % 2 != 0:
+        mid = int(len(nums) / 2) 
+        res.append(nums[mid] * nums[mid])
+    return res
+
+print(Multiplication_elements(nums))
